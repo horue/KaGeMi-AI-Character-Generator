c4ed3186442a16d2b0359cac0af0811333bad19e
Jorge <jorgejapao00@gmail.com>
2024-04-26 19:39:50 -0300

    Entrys and generate button added to main screen

--- GUI/gui.py
+++ GUI/gui.py
@@ -2,16 +2,27 @@ import customtkinter as ct
 
 def initial(root):
     e1=ct.CTkEntry(root, placeholder_text="Character name")
-    e1.pack(pady=100, padx=100)
+    e1.pack(pady=10)
 
     e2=ct.CTkEntry(root, placeholder_text="Character age")
-    e2.pack()
+    e2.pack(pady=10)
+
+    e3=ct.CTkEntry(root, placeholder_text="Character personality")
+    e3.pack(pady=10)
+
+    e4=ct.CTkEntry(root, placeholder_text="Background")
+    e4.pack(pady=10)
+
+    b1=ct.CTkButton(root, text="Generate")
+    b1.pack()
+
+
     print(1)
 
 
 def main():
     root = ct.CTk()
-    root.geometry("600x450")
+    root.geometry("600x590")
     root.title("Character Generator")
 
     initial(root)
